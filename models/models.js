2f63666402c0426e2a9b7f5ae370e0ced4427380
Antonio Galán <antonio.galanlaso@gmail.com>
2015-07-28 00:37:27 +0200

    version con base de datos

---
+++ models/models.js
@@ -0,0 +1,43 @@
+var path = require('path');
+
+var url = (process.env.DATABASE_URL || 'sqlite://:@:/').match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name		= (url[6] || null);
+var user 		= (url[2] || null);
+var pwd 		= (url[3] || null);
+var protocol 	= (url[1] || null);
+var dialect 	= (url[1] || null);
+var port 		= (url[5] || null);
+var host	 	= (url[4] || null);
+var storage 	= process.env.DATABASE_STORAGE;
+
+// Cargar modelo ORM
+var Sequelize = require('sequelize');
+
+// Usar DDBB SQlite:
+var sequelize = new Sequelize(DB_name,user,pwd,
+	{
+		dialect	: protocol,
+		protocol: protocol,
+		port    : port,
+		host    : host,
+		storage : storage,
+		omitNull: true 
+	});
+
+// Importar la definicion de la tabla Quiz en quiz.js
+var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+
+exports.Quiz = Quiz // exportar la definición de la tabla quiz
+
+// sequelize.sync() crea e instancia tabla de preguntas en DB
+sequelize.sync().then(function(){
+	Quiz.count().then(function(count){
+		if (count === 0){
+			Quiz.create({
+				pregunta: 'Capital de italia',
+				respuesta: 'Roma'
+			})
+			.then(function(){console.log('Base de datos inicializada.')});
+		};
+	});
+});
