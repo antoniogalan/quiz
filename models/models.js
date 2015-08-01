53020f3144b92d7e45e7f1d88eb3672a5de5d19b
Antonio Galán <antonio.galanlaso@gmail.com>
2015-08-01 14:35:50 +0200

    Carga de tabla de preguntas

--- models/models.js
+++ models/models.js
@@ -34,8 +34,12 @@ sequelize.sync().then(function(){
 	Quiz.count().then(function(count){
 		if (count === 0){
 			Quiz.create({
-				pregunta: 'Capital de italia',
+				pregunta: 'Capital de Italia',
 				respuesta: 'Roma'
+			});
+			Quiz.create({
+				pregunta: 'Capital de Portugal',
+				respuesta: 'Lisboa'
 			})
 			.then(function(){console.log('Base de datos inicializada.')});
 		};
