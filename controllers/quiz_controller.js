7eb720d44a723bdf3634b231a1aa146fca36a975
Antonio Galán <antonio.galanlaso@gmail.com>
2015-08-03 19:05:23 +0200

    Validación de entradas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -16,7 +16,7 @@ exports.load = function (req, res, next, quizId){
 
 // GET /quizes/:id
 exports.show = function (req,res){
-	res.render('quizes/show',{quiz: req.quiz});
+	res.render('quizes/show',{quiz: req.quiz, errors: []});
 };
 
 // GET /quizes/:id/answer
@@ -25,7 +25,7 @@ exports.answer = function (req,res){
 	if (req.query.respuesta === req.quiz.respuesta){
 		resultado = 'Correcto'
 	}
-	res.render('quizes/answer',{quiz: req.quiz, respuesta: resultado});
+	res.render('quizes/answer',{quiz: req.quiz, respuesta: resultado, errors: []});
 };
 
 // GET /quizes/
@@ -39,7 +39,7 @@ exports.index = function (req,res){
 	console.log("Texto a buscar:" + texto_buscar);
 	models.Quiz.findAll({where: ["pregunta like ?", texto_buscar],order: [['pregunta', 'ASC']]}).then(
 		function(quizes){
-			res.render('quizes/index.ejs',{quizes: quizes});
+			res.render('quizes/index.ejs',{quizes: quizes, errors: []});
 		}
 	).catch(function(error){next (error);});
 };
@@ -50,7 +50,7 @@ exports.new = function (req,res){
 		{pregunta: "Pregunta", respuesta: "Respuesta"}
 	);
 
-	res.render('quizes/new',{quiz: quiz});
+	res.render('quizes/new',{quiz: quiz, errors: []});
 };
 
 // POST /quizes/create
@@ -58,9 +58,19 @@ exports.create = function(req, res) {
 	var quiz = models.Quiz.build( req.body.quiz );
 
 	// guardar en DB los campos pretunta y respuesta de quiz
-	quiz.save({fields: ["pregunta","respuesta"]}).then(function () {
-		res.redirect('/quizes');
-	}) // Redireccion HTTP (URL relativo) lista de preguntas
+	quiz
+	.validate()
+	.then(
+		function (err) {
+			if (err) {
+				res.render('quizes/new',{quiz: quiz, errors: err.errors});
+			} else {
+				quiz
+				.save({fields: ["pregunta","respuesta"]})
+				.then(function () { res.redirect('/quizes');}) // Redireccion HTTP (URL relativo) lista de preguntas
+			}
+		}
+	);		
 };
 
 // GET /author
