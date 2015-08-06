6122e92c42f3239b72a348bc978386828594dd1a
Antonio Galán <antonio.galanlaso@gmail.com>
2015-08-06 10:27:29 +0200

    Moderación de comentarios

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -1,5 +1,23 @@
 var models = require('../models/models.js');
 
+// Autoload: id de comentarios
+exports.load = function (req, res, next, commentId){
+	console.log('Valor de commentId es:' + commentId);
+	models.Comment.find({
+			where: {
+				id: Number(commentId)
+			}
+		}).then(
+		function(comment){
+			if (comment) {
+				req.comment = comment;
+				next();
+			}else { next (new Error("No existe el commentId=" + commentId)); }
+		}
+	).catch (function(error){next(error);});
+	
+};
+
 // GET /quizes/:quizId/comments/new
 exports.new = function(req, res) {
 	res.render('comments/new.ejs',{quizid: req.params.quizId, errors: []});
@@ -28,4 +46,12 @@ exports.create = function(req, res) {
 			}
 		}
 	).catch(function(error){next(error)});
+};
+
+exports.publish = function(req, res){
+	req.comment.publicado = true;
+
+	req.comment.save({fields: ["publicado"]})
+		.then(function(){res.redirect('/quizes/' + req.params.quizId);})
+		.catch(function(error){next(error)});
 };
--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,7 @@ router.get('/', function(req, res) {
 
 // Autoload de comandos con :quizId
 router.param('quizId',quizController.load); // autoload quizId
+router.param('commentId',commentController.load); // autoload commentId
 
 
 // Definicion de rutas de session
@@ -32,6 +33,7 @@ router.delete('/quizes/:quizId(\\d+)'	,sessionController.loginRequired, quizCont
 
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 router.get('/author',quizController.author);
 
